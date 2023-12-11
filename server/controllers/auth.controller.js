e0269b1dd6b52910ff119f41768e244cc6b0cd1b
devyanshpopli <devyanshpopli40@gmail.com>
2023-12-11 14:51:20 +0530

    worked on hashing password and generating token

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -4,9 +4,15 @@ const {authService} =require('../services')
 const authController={
     async register(req,res,next){
        try {
-       
+       const {email,password} = req.body;
+       const user = await authService.createUser(email,password);
+       const token = await authService.genAuthToken(user);
+
+       res.cookie('x-access-token',token).status(200).send({
+        user,token
+       })
        } catch (error) {
-        
+        console.log(error);
        }
     },
     async signin(req,res,next){
--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -1,15 +1,33 @@
 //just js file that exports all the function
+const {User} = require('../models/user');
 
 
-
-const helloService = ()=>{
+const createUser = async(email,password)=>{
     try {
-        return 'HELLO';
+
+        if(await User.emailTaken(email)){
+            console.log('Email already on DB');
+            // throw error;
+        }
+
+       const user = new User({
+        email,
+        password
+       });
+       await user.save();
+       return user;
     } catch (error) {
-        
+        throw error;
     }
 }
 
+
+const genAuthToken = (user) =>{
+    const token = user.generateAuthToken();
+    return token;
+}
+
 module.exports = {
-    helloService
+    createUser,
+    genAuthToken
 }
